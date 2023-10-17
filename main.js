2b6742f79c373bac27ab335df565dbb62b78b2e1
Alex Jafet Lopez Curruchiche <alexlopez.curru@gmail.com>
2023-10-17 13:53:01 -0800

    Codigo boton

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-const {Client, EmbedBuilder} = require("discord.js")
+const {Client, EmbedBuilder, ButtonBuilder, ActionRowBuilder} = require("discord.js")
 const client = new Client({intents: [3276799]})
+client.on("ready", async()=>{
+    console.log("El bot ya esta listo")
+})
 
 const config = require("./config.json")
 
 client.on("messageCreate", async message =>{
-    if(message.content == "!hola"){
-        const embed = new EmbedBuilder()
-        .setTitle("Titulo")
-        .setURL("https://imgur.com/gallery/mXnwmvG")
-        .setDescription("Description")
-        .setAuthor({
-            name: "Tu nombre aqui",
-            iconURL: "https://i.imgur.com/Q9D499j.png",
-            url: "https://i.imgur.com/Q9D499j.png"
-        })
-        .setThumbnail("https://i.imgur.com/Q9D499j.png")
-        .setImage("https://i.imgur.com/Q9D499j.png")
-        .setFooter({
-            text: "Hola",
-            iconURL: "https://i.imgur.com/Q9D499j.png"
+    if(message.content == "!Acciones"){
+                const btnHeroe = new ActionRowBuilder()
+        .addComponents([
+            new ButtonBuilder()
+            .setLabel("Descubre que heroe eres")
+            .setEmoji("🍏")
+            .setStyle("Primary")
+            .setCustomId("btnheroe")
+        ])
+
+        //
+       
+        message.channel.send({
+            components: [btnHeroe],
            
         })
-        .setTimestamp()
-        .setFields({
-            name: "Nombre",
-            value: "Alex",
-            inline: true
-        }, {
-            name: "Nombre",
-            value: "Lucas",
-            inline: true
-        })
-       
-
-        message.channel.send({
-            embeds: [embed]
-        })
+    
     }
+   
 })
 
+client.on("interactionCreate", async interaction =>{
+ 
+    if(interaction.customId = "btnheroe"){
+        interaction.reply({
+            content: "Hola"
+        }
+        )
+    }
+ 
+})
+
+
 client.login(config.token)
-console.log("El bot ya esta listo!");
